5e33033ce1eb06841a330f05344e2726100ffd2c
ion-storm <defconoii@gmail.com>
2014-08-07 21:08:09 -0400

    low temp threshold and freqs to lower temp quicker & avoid overheat

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -25,7 +25,7 @@
 #include <linux/of.h>
 #include <mach/cpufreq.h>
 
-unsigned int temp_threshold = 70;
+unsigned int temp_threshold = 68;
 module_param(temp_threshold, int, 0755);
 
 static struct thermal_info {
@@ -41,10 +41,10 @@ static struct thermal_info {
 };
 
 enum thermal_freqs {
-	FREQ_HELL 	  = 702000,
-	FREQ_VERY_HOT = 918000,
+	FREQ_HELL 	  = 648000,
+	FREQ_VERY_HOT = 810000,
 	FREQ_HOT 	  = 1026000,
-	FREQ_WARM 	  = 1296000,
+	FREQ_WARM 	  = 1242000,
 };
 
 enum threshold_levels {
